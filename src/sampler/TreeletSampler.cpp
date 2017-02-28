fccb4381e989b195864ebc5c07b4755a9691ac1c
Steven <devnull@localhost>
2017-02-28 14:28:13 +0100

    Sampler optimizations (stop early when a decomposition is found). Removed ReservoirSampler.

--- src/sampler/TreeletSampler.cpp
+++ src/sampler/TreeletSampler.cpp
@@ -3,7 +3,6 @@
 //
 
 #include "TreeletSampler.h"
-#include "ReservoirSampler.h"
 
 bool TreeletSampler::sample_rooted_occurrence(const Treelet& t, const UndirectedGraph::vertex_t u, UndirectedGraph::vertex_t* occurrence)
 {
@@ -14,44 +13,74 @@ bool TreeletSampler::sample_rooted_occurrence(const Treelet& t, const Undirected
     if(t.number_of_vertices() == 1)
         return true;
 
-    ReservoirSampler<std::pair<Treelet, UndirectedGraph::vertex_t> > sampler(std::make_pair(Treelet::invalid_treelet, 0), rng);
+    //ReservoirSampler<std::pair<Treelet, UndirectedGraph::vertex_t> > sampler(std::make_pair(Treelet::invalid_treelet, 0), rng);
 
     Treelet split = t.split_child();
     assert(!split.is_colored());
 
-    const TreeletTable *table = table_collection->get_table(split.number_of_vertices());
+    const TreeletTable *table = table_collection->get_table(t.number_of_vertices());
+    const TreeletTable *split_table = table_collection->get_table(split.number_of_vertices());
+
+    TreeletTable::treelet_count_t count = table->get_count(u, t);
+    if(count==0)
+        return false;
+
+    mul_overflow(count, t.normalization_factor(), &count);
+    TreeletTable::treelet_count_t r = rng->random_uint64(0,  count);
+
+    Treelet child_treelet = Treelet::invalid_treelet;
+    UndirectedGraph::vertex_t child_vertex=0;
 
     const UndirectedGraph::vertex_t *neighbors = graph->neighbors(u);
     for(UndirectedGraph::vertex_t d = 0; d < graph->degree(u); ++d)
     {
         const UndirectedGraph::vertex_t v = neighbors[d];
-        for(TreeletTable::const_iterator it = table->begin(v, split); it != table->end(v); ++it)
+        for(TreeletTable::const_iterator it = split_table->begin(v, split); it != split_table->end(v); ++it)
         {
             const Treelet& t2 = it.treelet();
             if(t2.get_structure() != split.get_structure())
                 break;
 
-            if((t2.get_colors() & ~t.get_colors()) == 0 )
-            {
-                Treelet complement = t.complement(t2);
-                TreeletTable::treelet_count_t c = table_collection->get_table(complement.number_of_vertices())->get_count(u, complement);
+            if( t2.get_colors() & ~t.get_colors() )
+                continue;
 
-                assert(it.count()!=0);
-                sampler.feed(std::make_pair(t2, v), c * it.count());
+            Treelet complement = t.complement(t2);
+            TreeletTable::treelet_count_t c = table_collection->get_table(complement.number_of_vertices())->get_count(u, complement);
+
+            assert(it.count()!=0);
+            //sampler.feed(std::make_pair(t2, v), c * it.count());
+            assert(c*it.count() <= count);
+
+#ifndef NDEBUG
+            count -= c*it.count();
+
+            if(!child_treelet.is_valid())
+#endif
+            {
+                if (r >= c * it.count())
+                    r -= c*it.count();
+                else
+                {
+                    child_treelet = t2;
+                    child_vertex = v;
+#ifdef NDEBUG
+                    goto end_loop; //Children found, exit early
+#endif
+                }
             }
         }
     }
 
-    assert(sampler.get_total_weight()!=0);
-    assert(table_collection->get_table(t.number_of_vertices())->get_count(u, t) * t.normalization_factor() == sampler.get_total_weight());
+#ifdef NDEBUG
+    end_loop:
+#endif
 
-    auto child = sampler.get_sample();
-    if(!child.first.is_valid())
-        return false;
+    assert(count == 0);
+    assert(child_treelet.is_valid());
 
-    Treelet complement = t.complement(child.first);
+    Treelet complement = t.complement(child_treelet);
 
-    return (complement.is_singleton() || sample_rooted_occurrence(complement, u, occurrence + child.first.number_of_vertices()) ) &&
-            sample_rooted_occurrence(child.first, child.second, occurrence+1);
+    return (complement.is_singleton() || sample_rooted_occurrence(complement, u, occurrence + child_treelet.number_of_vertices()) ) &&
+            sample_rooted_occurrence(child_treelet, child_vertex, occurrence+1);
 }
 
