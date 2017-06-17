af4cdf647a8582268786a6ac63f0f8c22d65b47c
Steven <devnull@localhost>
2017-06-17 20:54:15 +0200

    Make sure the vertex map memory is released before writing

--- scripts/loe_to_graph.cpp
+++ scripts/loe_to_graph.cpp
@@ -5,18 +5,17 @@
 #include<set>
 
 const std::string whitespace = " \t\r";
-std::map<const std::string, uint32_t> vertices;
 std::vector< std::set<uint32_t> > edges;
 uint32_t n=0;
 uint64_t m=0;
 
-uint32_t getID(const std::string& name)
+uint32_t getID(std::map<const std::string, uint32_t>& map, const std::string& name)
 {
-        auto it = vertices.find(name);
-        if(it!=vertices.end())
+        auto it = map.find(name);
+        if(it!=map.end())
                 return it->second;
-        
-        vertices[name] = n;
+
+        map[name] = n;
         edges.emplace_back();
         std::cerr << name << std::endl;
         return n++;
@@ -45,14 +44,15 @@ bool split(const std::string& str, std::string& first, std::string& second)
 
 void read()
 {
+        std::map<const std::string, uint32_t> vertices;
         std::string line, first, second;
         while(std::getline(std::cin, line))
         {
                 if(!split(line, first, second))
                         continue;
                 
-                uint32_t u = getID(first);
-                uint32_t v = getID(second);
+                uint32_t u = getID(vertices, first);
+                uint32_t v = getID(vertices, second);
                 
                 if(u==v)
                         continue;
@@ -69,6 +69,7 @@ void read()
         }
 
         edges.shrink_to_fit();
+        delete &vertices;
 }
 
 void write()
