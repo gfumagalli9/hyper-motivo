75aec692e4da7e20f17fb62107545271d4229ae8
Steven <devnull@localhost>
2017-06-24 22:11:30 +0200

    Bugfix

--- src/builder/TreeletTableBuilder.cpp
+++ src/builder/TreeletTableBuilder.cpp
@@ -98,7 +98,7 @@ void TreeletTableBuilder::do_build_st()
     {
         report_progress(u);
 
-        if(store_0_only && lower->get_table(1)->begin(u).treelet().get_colors()==1)
+        if(store_0_only && lower->get_table(1)->begin(u).treelet().get_colors()!=1)
             continue;
 
         table_t table;
@@ -128,7 +128,7 @@ void TreeletTableBuilder::do_build_mt(std::atomic<UndirectedGraph::vertex_t> *at
         {
             report_progress(u);
 
-            if(store_0_only && lower->get_table(1)->begin(u).treelet().get_colors()==1)
+            if(store_0_only && lower->get_table(1)->begin(u).treelet().get_colors()!=1)
             {
                 batch[u-start] = std::make_pair(nullptr, 0);
                 continue;
