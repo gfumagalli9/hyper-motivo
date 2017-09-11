a0679e3acb8de1c11c99951950d3cec29fc8a934
Steven <devnull@localhost>
2017-09-11 02:10:28 +0200

    Add last commit date to version

--- hg_id.cmake
+++ hg_id.cmake
@@ -1,7 +1,13 @@
 execute_process(COMMAND hg id -i -b WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} TIMEOUT 5 RESULT_VARIABLE HG_ID_RESULT OUTPUT_VARIABLE HG_ID OUTPUT_STRIP_TRAILING_WHITESPACE)
 if(NOT HG_ID_RESULT EQUAL 0)
-    set(HG_ID unknown)
+    set(HG_ID "unknown")
+else()
+    execute_process(COMMAND hg log -b . -r . --template "{date(localdate(date,0),'%Y-%m-%dT%H:%MZ')}" WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} TIMEOUT 5 RESULT_VARIABLE HG_DATE_RESULT OUTPUT_VARIABLE HG_DATE OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(HG_DATE_RESULT EQUAL 0)
+        set(HG_ID "${HG_ID} ${HG_DATE}")
+    endif()
 endif()
+
 
 message(STATUS "Detected hg id: ${HG_ID}")
 
