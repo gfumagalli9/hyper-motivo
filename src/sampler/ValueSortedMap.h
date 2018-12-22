f81fe5c8e57835143bf607c9b924042ddf276b33
Steven <devnull@localhost>
2018-12-22 08:11:15 +0100

    Fixed relative path for STL include in ValueSortedMap.h

--- src/sampler/ValueSortedMap.h
+++ src/sampler/ValueSortedMap.h
@@ -8,7 +8,7 @@
 #ifndef SRC_COMMON_VALUESORTEDMAP_H_
 #define SRC_COMMON_VALUESORTEDMAP_H_
 
-#include "../../../../../../usr/include/c++/8/map"
+#include <map>
 
 /**
  * A map that keeps the elements sorted in nondecreasing order of value V.
